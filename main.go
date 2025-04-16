c24ac8e7a8321b590721dc92bd4a33d0bef6adbd
quockhanhcao <quockhanhcao99@gmail.com>
2025-04-17 00:04:23 +0700

    feat: Setting up tcp listener, reading from tcp connection

--- main.go
+++ main.go
@@ -5,33 +5,42 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("messages.txt")
+	tcpListener, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error listening for TCP: %s\n", err.Error())
 	}
-	linesChannel := getLinesChannel(file)
-	for line := range linesChannel {
-		fmt.Printf("read: %s\n", line)
+	defer tcpListener.Close()
+	for {
+		connection, err := tcpListener.Accept()
+		if err != nil {
+            log.Fatalf("Error accepting TCP connection: %s\n", err.Error())
+		}
+		fmt.Println("Connection accepted from:", connection.RemoteAddr())
+		linesChannel := getLinesChannel(connection)
+		for line := range linesChannel {
+			fmt.Println(line)
+		}
+		fmt.Println("Connection closed from:", connection.RemoteAddr())
 	}
 }
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
 	channel := make(chan string)
 	go func() {
-        defer f.Close()
-        defer close(channel)
+		defer f.Close()
+		defer close(channel)
 		currentLine := ""
 		for {
 			buffer := make([]byte, 8)
 			n, err := f.Read(buffer)
 			if err != nil {
 				if currentLine != "" {
-                    channel <- currentLine
+					channel <- currentLine
 				}
 				if errors.Is(err, io.EOF) {
 					break
