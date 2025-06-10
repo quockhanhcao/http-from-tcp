080f47f58cc093ac282a70887f3119a4444f78f8
quockhanhcao <quockhanhcao99@gmail.com>
2025-06-11 00:03:52 +0700

    feat: HTTP handler

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/quockhanhcao/http-from-tcp/internal/request"
+	"github.com/quockhanhcao/http-from-tcp/internal/response"
 	"github.com/quockhanhcao/http-from-tcp/internal/server"
 )
 
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -23,4 +26,20 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	log.Println("Server gracefully stopped")
+}
+
+func handler(w io.Writer, req *request.Request) *server.HandlerError {
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message:    "Your problem is not my problem\n",
+		}
+	} else if req.RequestLine.RequestTarget == "/myproblem" {
+		return &server.HandlerError{
+			StatusCode: response.InternalServerErrorCode,
+			Message:    "Woopsie, my bad\n",
+		}
+	}
+	w.Write([]byte("All good, frfr\n"))
+	return nil
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,16 +1,35 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"sync/atomic"
 
+	"github.com/quockhanhcao/http-from-tcp/internal/request"
 	"github.com/quockhanhcao/http-from-tcp/internal/response"
 )
+
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
+func (he HandlerError) WriteErrorHandler(w io.Writer) {
+	response.WriteStatusLine(w, he.StatusCode)
+	messageBytes := []byte(he.Message)
+	headers := response.GetDefaultHeaders(len(messageBytes))
+	response.WriteHeaders(w, headers)
+	w.Write(messageBytes)
+}
 
 type Server struct {
 	listener net.Listener
 	closed   atomic.Bool
+	handler  Handler
 }
 
 // Close the listener and the server
@@ -24,46 +43,59 @@ func (s *Server) Close() error {
 
 // uses a loop to .Accept new connections as they come in, handles each one
 // in a separate goroutine
-func (s *Server) listen() {
-	if s.closed.Load() {
-		return
-	}
+func (s *Server) listen(handler Handler) {
 	for {
 		conn, err := s.listener.Accept()
 		if err != nil {
+			if s.closed.Load() {
+				return
+			}
 			fmt.Println("error accepting connection: %w", err)
 			continue
 		}
-		go s.handle(conn)
+		go s.handle(conn, handler)
 	}
 }
 
 // handle a single connection by writing the response and closing the connection
-func (s *Server) handle(conn net.Conn) {
+func (s *Server) handle(conn net.Conn, handler Handler) {
 	defer conn.Close()
-	// handle the connection
-	headers := response.GetDefaultHeaders(0)
-	err := response.WriteStatusLine(conn, response.StatusOK)
-    if err != nil {
-        fmt.Println("error writing status line: %w", err)
-    }
-	err = response.WriteHeaders(conn, headers)
+	// handle the request
+	request, err := request.RequestFromReader(conn)
 	if err != nil {
-		fmt.Println("error writing headers: %w", err)
+		handlerError := &HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message:    err.Error(),
+		}
+		handlerError.WriteErrorHandler(conn)
+		return
 	}
+	buf := &bytes.Buffer{}
+	handlerError := handler(buf, request)
+	if handlerError != nil {
+		handlerError.WriteErrorHandler(conn)
+		return
+	}
+	b := buf.Bytes()
+	// write the response if succeeded
+	response.WriteStatusLine(conn, response.StatusOK)
+	headers := response.GetDefaultHeaders(len(b))
+	response.WriteHeaders(conn, headers)
+	conn.Write(b)
 }
 
 // Create a net.Listener and returns new Server instance
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return nil, err
 	}
 	server := &Server{
 		listener: listener,
+		handler:  handler,
 	}
 	server.closed.Store(false)
 
-	go server.listen()
+	go server.listen(handler)
 	return server, nil
 }
