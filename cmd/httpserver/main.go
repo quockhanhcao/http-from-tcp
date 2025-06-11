9d453c3e9bee6d9bd05b38ddf5474be70d452f5f
quockhanhcao <quockhanhcao99@gmail.com>
2025-06-11 23:46:52 +0700

    feat: Implementing chunk encoding for responses

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
+
 	"github.com/quockhanhcao/http-from-tcp/internal/headers"
 	"github.com/quockhanhcao/http-from-tcp/internal/request"
 	"github.com/quockhanhcao/http-from-tcp/internal/response"
@@ -28,12 +32,63 @@ func main() {
 }
 
 func handler(w *response.Writer, req *request.Request) {
+	if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/") {
+		proxyHandler(w, req)
+		return
+	}
 	if req.RequestLine.RequestTarget == "/yourproblem" {
 		badRequestHandler(w)
+		return
 	} else if req.RequestLine.RequestTarget == "/myproblem" {
 		internalServerErrorHandler(w)
+		return
 	} else {
 		successRequestHandler(w)
+		return
+	}
+}
+
+func proxyHandler(w *response.Writer, req *request.Request) {
+	requestParams := strings.TrimPrefix(req.RequestLine.RequestTarget, "/httpbin/")
+	fmt.Println("Proxying request to httpbin with params:", requestParams)
+	resp, err := http.Get("https://httpbin.org/" + requestParams)
+	if err != nil {
+		log.Printf("Error making request to httpbin: %v", err)
+		internalServerErrorHandler(w)
+		return
+	}
+	defer resp.Body.Close()
+
+	// for the response to client
+	w.WriteStatusLine(response.StatusOK)
+	headers := headers.GetDefaultHeaders(0)
+	headers.Remove("Content-Length")
+	headers.OverrideHeadersByKey("Transfer-Encoding", "chunked")
+	w.WriteHeaders(headers)
+
+	buffer := make([]byte, 1024)
+	for {
+		n, err := resp.Body.Read(buffer)
+		fmt.Println("Read ", n, "bytes")
+		if n > 0 {
+			_, err = w.WriteChunkedBody(buffer[:n])
+			if err != nil {
+				fmt.Printf("Error writing chunked body: %v", err)
+				break
+			}
+		}
+		if n == 0 {
+			fmt.Println("Reached end of response body from httpbin")
+			break
+		}
+		if err != nil {
+			fmt.Printf("Error reading response body from httpbin: %v", err)
+			break
+		}
+	}
+	_, err = w.WriteChunkedBodyDone()
+	if err != nil {
+		fmt.Printf("Error writing chunked body done: %v", err)
 	}
 }
 
